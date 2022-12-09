ef488b9be598e482b771b95b277a5e5f47f9e1a3
Vitoriano <vitorianoernandes2012@gmail.com>
2022-12-08 21:33:18 -0300

    Criando o contexto de navegação

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,31 @@
+
+import { createStackNavigator, StackNavigationProp } from "@react-navigation/stack";
+
+import { SignIn } from "@screens/SignIn";
+import { SignUp } from "@screens/SignUp";
+
+type AuthRoutes = {
+  signIn: undefined;
+  signUp: undefined;
+}
+
+export type AuthNavigatorRoutesProps = StackNavigationProp<AuthRoutes>;
+
+const { Navigator, Screen } = createStackNavigator<AuthRoutes>();
+
+export function AuthRoutes() {
+  return (
+    <Navigator screenOptions={{ headerShown: false}}>
+      <Screen 
+        name="signIn"
+        component={SignIn}
+      />
+
+      <Screen 
+        name="signUp"
+        component={SignUp}
+      />
+
+    </Navigator>
+  );
+}
