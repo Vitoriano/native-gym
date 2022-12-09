5996e18a1f26ef10520d4e23d776d443699dc4e2
Vitoriano <vitorianoernandes2012@gmail.com>
2022-12-08 21:49:57 -0300

    Rotas publicas

--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -1,11 +1,21 @@
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import { useNavigation } from "@react-navigation/native";
 
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
+
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
+
 export function SignUp(){
+
+  const navigation = useNavigation();
+
+  function handleGoBack(){
+    navigation.goBack();
+  }
+
   return (
     <ScrollView 
       contentContainerStyle={{ flexGrow: 1}} 
@@ -51,6 +61,7 @@ export function SignUp(){
             title="Voltar para o login" 
             variant="outline"
             mt={24}
+            onPress={handleGoBack}
           />
 
       </VStack>
