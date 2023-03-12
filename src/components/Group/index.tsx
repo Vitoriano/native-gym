46b8e235b3aab2c3b5aa6074f7c117784015474b
Vitoriano <vitorianoernandes2012@gmail.com>
2023-03-11 23:27:09 -0300

    Listando os grupos

---
+++ src/components/Group/index.tsx
@@ -0,0 +1,37 @@
+import { Pressable, Text, IPressableProps } from "native-base";
+
+type Props = IPressableProps & {
+  name: string;
+  isActive: boolean;
+}
+
+export function HomeGroup({ name, isActive, ...rest }: Props) {
+  return (
+    <Pressable
+      mr={3}
+      w={24}
+      h={10}
+      bg="gray.600"
+      justifyContent="center"
+      alignItems="center"
+      overflow="hidden"
+      rounded="md"
+      isPressed={isActive}
+      _pressed={{
+        borderColor: "green.500",
+        borderWidth: 1
+      }}
+      {...rest}
+    >
+      <Text
+        color="gray.200"
+        textTransform="uppercase"
+        fontSize="xs"
+        fontWeight="bold"
+      >
+        {name}
+      </Text>
+    </Pressable>
+    
+  );
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,38 @@
 
+import { useState } from "react";
+
+import { HomeGroup } from "@components/Group";
 import { HomeHeader } from "@components/Header";
-import { VStack } from "native-base";
+import { FlatList, HStack, VStack } from "native-base";
 
 export function Home() {
+  
+  const [groups, setGroups] = useState(["Costas", "Bíceps", "Trícips", "Ombros"]);
+  const [groupSelected, setGroupSelected] = useState("costa");
+  
   return (
 
     <VStack flex={1}>
+
       <HomeHeader />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={ item => item }
+        renderItem={({ item }) => (
+          <HomeGroup 
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => { setGroupSelected(item) }}
+          />
+        )}
+       horizontal={true}
+       my={10}
+       showsHorizontalScrollIndicator={false}
+       _contentContainerStyle={{ px: 8 }}
+       maxH={10}
+      />
+
     </VStack>
 
   );
