a4bc842b72e5e5ecd2c10a9b83a7107568e96c20
Vitoriano <vitorianoernandes2012@gmail.com>
2023-03-23 23:18:21 -0300

    Component HistoryCrad

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,11 +1,42 @@
-import { Center, Text } from "native-base";
+import { HistoryCard } from "@components/HistoryCard";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { Center, Heading, SectionList, Text, VStack } from "native-base";
+import { useState } from 'react';
 
 export function History() {
+
+  const [exercices, setExcercices] = useState([
+    {
+      title: '05.03.2023',
+      data: ['Puxada Frontal', 'Remada']
+    },
+    {
+      title: '11.03.2023',
+      data: ['Agachamento']
+    }
+  ]);
+
   return (
 
-    <Center flex={1}>
-      <Text>History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title='Historico de Exercicos'/>
+
+      <SectionList 
+        sections={exercices}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading color='gray.200' fontSize='md' mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={3}
+      />
+      
+      
+    </VStack>
 
   );
 }
