e6b70d935858e02f186e422df94a8670a7e73ab3
Vitoriano <vitorianoernandes2012@gmail.com>
2023-03-26 22:34:01 -0300

    Criando os inputs da edicao do perfil

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
-import { Center, ScrollView, VStack, Skeleton } from "native-base";
+import { Center, ScrollView, VStack, Skeleton, Text } from "native-base";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/Avatar";
+import { TouchableOpacity } from "react-native";
+import { Input } from "@components/Input";
 
 const PHOTO_SIZE = 33;
 
@@ -36,6 +38,24 @@ export function Profile() {
               mr={4}
             />
           }
+
+          <TouchableOpacity>
+            <Text color='green.500' fontWeight='bold' fontSize='md' mt={2} mb={8}>
+              Altera Foto
+            </Text>
+          </TouchableOpacity>
+
+          <Input 
+            bg='gray.600'
+            placeholder="nome"
+          />
+            
+          <Input 
+            bg='gray.600'
+            placeholder="email.com.br"
+            isDisabled
+          />
+
         </Center>
        
       </ScrollView>
