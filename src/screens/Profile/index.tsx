51cf2a9f3b0ef825ab11bbece3f2319ed2a34bc6
Vitoriano <vitorianosouza@movida.com.br>
2023-08-26 22:48:07 -0300

    Handle with photos and galery of photos

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import { Center, ScrollView, VStack, Skeleton, Text, Heading } from "native-base";
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast } from "native-base";
+import * as ImagePick from "expo-image-picker";
+import * as FileSystem from 'expo-file-system'
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/Avatar";
-import { TouchableOpacity } from "react-native";
+import { Alert, TouchableOpacity } from "react-native";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
@@ -12,6 +14,57 @@ const PHOTO_SIZE = 33;
 export function Profile() {
 
   const [isLoadingPhoto, setIsLoadingPhoto] = useState(true);
+  const [photo, setPhoto] = useState('https://github.com/Vitoriano.png');
+
+  const toast = useToast();
+
+  async function handleSelectPhoto() {
+    setIsLoadingPhoto(true);
+    try {
+      
+      const { status } = await ImagePick.requestMediaLibraryPermissionsAsync();
+
+      if (status !== 'granted') {
+        alert('Eita, precisamos de acesso as suas fotos...');
+        return;
+      }
+  
+      const result = await ImagePick.launchImageLibraryAsync({
+        allowsEditing: true,
+        quality: 1,
+        mediaTypes: ImagePick.MediaTypeOptions.Images,
+        aspect: [4, 4]
+      });
+  
+      
+      if (result.canceled) {
+        return;
+      }
+      
+      const photoInfo = await FileSystem.getInfoAsync(result.assets[0].uri);
+
+      if(photoInfo.size && (photoInfo.size / 1024 / 1024) > 1){
+      
+        return toast.show({
+          title: 'Imagem muito grande',
+          description: 'A imagem deve ter no máximo 1MB',
+          placement: 'top',
+          duration: 3000,
+          bgColor: 'red.500'
+        });
+        
+      }
+      
+      setPhoto(result.assets[0].uri);
+
+    } catch (error) {
+      console.log(error);
+    }finally{
+      setIsLoadingPhoto(false);
+    }
+
+   
+  }
 
   return (
 
@@ -33,14 +86,16 @@ export function Profile() {
             />
             :
             <UserPhoto 
-              source={{ uri: 'https://github.com/Vitoriano.png'}} 
+              source={{ uri: photo }} 
               alt="Imagem do usuario"
               size={PHOTO_SIZE}
               mr={4}
             />
           }
 
-          <TouchableOpacity>
+          <TouchableOpacity 
+            onPress={handleSelectPhoto}
+          >
             <Text color='green.500' fontWeight='bold' fontSize='md' mt={2} mb={8}>
               Altera Foto
             </Text>
