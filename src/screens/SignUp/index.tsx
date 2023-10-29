f4b036f94f3f972d14226dd7f8a7cde02d69d9aa
Vitoriano <vitorianosouza@movida.com.br>
2023-10-28 21:57:15 -0300

    Install Axios, Tramamento de erros, Interceptors

--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from "native-base";
 import { useNavigation } from "@react-navigation/native";
 import { useForm, Controller } from "react-hook-form";
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+
+import { api } from "@services/api";
 
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
@@ -11,6 +13,9 @@ import BackgroundImg from "@assets/background.png";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 import { type } from "os";
+import { Alert } from "react-native";
+
+import { AppError } from "@utils/AppError";
 
 type FormDataProps = {
   name: string;
@@ -26,6 +31,8 @@ const signUpSchema = yup.object({
 
 export function SignUp(){
 
+  const toast = useToast();
+
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     resolver: yupResolver(signUpSchema),
   });
@@ -37,22 +44,24 @@ export function SignUp(){
   }
 
   async function handleSignUp({name, email, password}: FormDataProps){
-   const response = await fetch('http://172.28.0.1:3333/users', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body : JSON.stringify({
+   
+    try {
+      const response = await api.post('/users', {
         name,
         email,
         password,
-      })
-    });
-
-    const data = await response.json();
-    console.log(data);
-    
+      });
+  
+    } catch (error) {
+      
+      const isAppError = error instanceof AppError;
+      toast.show({
+        title: isAppError ? error.message : 'Ocorreu um erro ao fazer cadastro',
+        duration: 3000,
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
   }
 
   return (
